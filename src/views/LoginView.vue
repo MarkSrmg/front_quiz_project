ebaca8d853497d365731a798296c56e91ef54ffb
asyawind <berestovaja@gmail.com>
2023-02-08 14:07:56 +0200

    LoginView is created

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,22 @@
 <template>
 
+<div class="row "/>
+
+
+  <div class="col-3">
+
+    <div class="form-floating mb-5">
+      <input type="email" class="form-control" id="floatingInput" placeholder="">
+      <label for="floatingInput">Username</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+  </div>
+
+
 </template>
 
 <script>
