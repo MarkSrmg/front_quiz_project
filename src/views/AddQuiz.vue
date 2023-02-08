6821bd57843a2b175dac338b39794dff529696df
Kivitagapeidus <S88rumail@gmail.com>
2023-02-08 14:48:23 +0200

    Taustapilt olemas

--- src/views/AddQuiz.vue
+++ src/views/AddQuiz.vue
@@ -7,11 +7,24 @@
           <input type="text" class="form-control" placeholder="Quiz name" aria-label="Username"
                  aria-describedby="basic-addon1">
         </div>
-        <div>
-          Create new flashcards
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+          <label class="form-check-label" for="flexRadioDefault1">
+            <h5>Create new flashcards</h5>
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+          <label class="form-check-label" for="flexRadioDefault1">
+            <h5>Create new quiz</h5>
+          </label>
+        </div>
+        <div  class="input-group mb-3">
+          <span class="input-group-text">Correct answers needed</span>
+          <input type="number" min="0" class="form-control" placeholder="0">
         </div>
         <div>
-          Create new quiz
+          <button type="button" class="btn btn-dark">Create</button>
         </div>
       </div>
     </div>
