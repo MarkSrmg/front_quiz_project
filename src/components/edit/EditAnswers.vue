c3cc03aadb845374d19875f472646e4acb87d178
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-02-20 15:47:03 +0200

    Edit quiz lehekülg toimib veidi juba :)

---
+++ src/components/edit/EditAnswers.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <div>
+      <div class="row justify-content-center">
+        <div class="col-4">
+          <AddAnswerText :answer-text-prop="answerRequest.answerText" :key="textComponentKey" ref="addAnswerText"
+                         @emitAddAnswerText="setAnswerText"/>
+          <div>
+            <div class="form-check">
+              <input class="css-checkbox" type="checkbox" value="" id="flexCheckDefault"
+                     v-model="answerRequest.answerIsCorrect">
+              <label class="form-check-label" for="flexCheckDefault"> This answer is correct
+              </label>
+            </div>
+          </div>
+          <div>
+            <ImageInput :picture-data-base64-prop="answerRequest.answerPicture" :key="imageComponentKey"
+                        @emitBase64Event="setAnswerPicture"/>
+          </div>
+          <div class="mb-3">
+            <button v-if="!isEdit" v-on:click="addAnswer" type="button" class="btn btn-success">Save answer</button>
+          </div>
+          <AlertDanger :message="message"/>
+          <div class="mb-3" v-if="isEdit">
+            <button v-on:click="editAnswer" type="button" class="btn btn-dark">Edit answer</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <EditQuizAnswerTable ref="quizAnswerTable"
+                             @emitClearAnswerRequestEvent="clearAnswerRequest"
+                             @emitAnswerEvent="setEditAnswerInputFields"
+                             :question-id="questionId"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+import AddAnswerText from "@/components/AddAnswer/AddAnswerText.vue";
+import ImageInput from "@/components/ImageInput.vue";
+import QuizAnswerTable from "@/components/AddAnswer/Quiz/QuizAnswerTable.vue";
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+import EditQuizAnswerTable from "@/components/edit/EditQuizAnswerTable.vue";
+
+export default {
+  name: 'EditAnswers',
+  components: {EditQuizAnswerTable, AlertDanger, QuizAnswerTable, ImageInput, AddAnswerText},
+  props: {
+    questionId: {}
+  },
+  data: function () {
+    return {
+      answerId: Number(this.$route.query.answerId),
+      answerRequest: {
+        answerId: 0,
+        answerText: '',
+        answerPicture: String,
+        answerIsCorrect: false
+      },
+      isShown: false,
+      message: '',
+      isEdit: false,
+      imageComponentKey: 0,
+      textComponentKey: 0,
+    }
+  },
+  methods: {
+    setEditAnswerInputFields: function (answer) {
+      this.answerRequest = answer
+      this.isEdit = true
+      this.textComponentKey += 1;
+      this.imageComponentKey += 1;
+
+    },
+    clearAnswerRequest: function () {
+      this.answerRequest.answerPicture = String
+      this.answerRequest.answerIsCorrect = false
+      this.answerRequest.answerText = ''
+      this.textComponentKey += 1;
+      this.imageComponentKey += 1;
+    },
+    setAnswerPicture: function (pictureDataBase64) {
+      this.answerRequest.answerPicture = pictureDataBase64
+    },
+    editAnswer: function () {
+      this.$refs.addAnswerText.emitAddAnswerText();
+      this.putAnswer()
+    },
+    addAnswer: function () {
+      this.message = ''
+      this.$refs.addAnswerText.emitAddAnswerText();
+      if (this.answerRequest.answerText === '' && this.answerRequest.answerPicture === String) {
+        this.message = 'Please enter your answer'
+      } else {
+        this.postAnswer();
+      }
+    },
+    setAnswerText: function (answerText) {
+      this.answerRequest.answerText = answerText
+    },
+    postAnswer: function () {
+      this.$http.post("/questions/answer", this.answerRequest, {
+            params: {
+              questionId: this.questionId
+            }
+          }
+      ).then(response => {
+        this.answerId = response.data.answerId;
+        this.isShown = true;
+        this.$refs.quizAnswerTable.getAllAnswers();
+        this.clearAnswerRequest()
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    putAnswer: function () {
+      this.$http.put("/questions/answer", this.answerRequest, {
+            params: {
+              answerId: this.answerRequest.answerId
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+        this.isEdit = false
+        this.$refs.quizAnswerTable.getAllAnswers();
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  }
+}
+</script>
+
+}
+</script>
---
+++ src/components/edit/EditQuizAnswerTable.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <div class="justify-content-center col-11">
+      <div class="justify-content-center col-12">
+        <hr class="solid">
+      </div>
+      <div>
+        <table class="table table-borderless text-white text fw-bold"
+               style="background-color: rgba(0, 0, 0, 0);">
+          <tbody>
+          <tr v-for="answer in answers" :key="answer.answerId">
+            <td style="max-width:50px;" class="tdbreak">
+              <div>
+                {{ answer.answerText }}
+              </div>
+            </td>
+            <td style="max-width:30px;">
+              <div v-if="answer.answerPicture !== null">
+                <img :src=answer.answerPicture class="img-thumbnail" alt="...">
+              </div>
+            </td>
+            <td title="edit answer">
+              <font-awesome-icon v-on:click="emitAnswer(answer)" icon="fa-solid fa-pencil" class="icon-hover"/>
+            </td>
+            <td title="delete answer">
+              <font-awesome-icon v-on:click="deleteAnswer(answer.answerId)" icon="fa-solid fa-trash-can"
+                                 class="icon-hover"/>
+            </td>
+            <td title="if this is checked, the answer is correct" style="max-width:21px;">
+              <div class="form-check" v-if="!answer.answerIsCorrect">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled>
+                <label class="form-check-label" for="defaultCheck2">
+                  right answer
+                </label>
+              </div>
+              <div class="form-check" v-if="answer.answerIsCorrect">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDisabled" disabled checked>
+                <label class="form-check-label" for="defaultCheck2">
+                  right answer
+                </label>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <!--    <div class="mb-3">-->
+      <!--      <button type="button" class="btn btn-dark">Previous question</button>-->
+      <!--    </div>-->
+      <AlertDanger :message="message"/>
+      <div class="mb-3">
+        <button v-on:click="finishQuiz" type="button" class="btn btn-dark">Finish quiz</button>
+      </div>
+      <div class="mb-3">
+        <button v-on:click="removeQuiz" type="button" class="btn btn-dark">Delete quiz</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+
+export default {
+  name: 'EditQuizAnswerTable',
+  components: {AlertDanger},
+  props: {
+    questionId: {}
+  },
+  data: function () {
+    return {
+      quizId: Number(this.$route.query.quizId),
+      answers: [
+        {
+          answerId: 0,
+          answerText: '',
+          answerPicture: '',
+          answerIsCorrect: false
+        }
+      ],
+      message: ''
+    }
+  },
+  methods: {
+    emitAnswer: function (answer) {
+      let answerString = JSON.stringify(answer)
+      this.$emit('emitAnswerEvent', JSON.parse(answerString))
+    },
+    finishQuiz: function () {
+      let hasCorrectAnswer = false;
+      for (let i = 0; i < this.answers.length; i++) {
+        if (this.answers[i].answerIsCorrect === true) {
+          hasCorrectAnswer = true;
+          break;
+        }
+      }
+      if (!hasCorrectAnswer) {
+        this.message = 'At least one answer needs to be set as correct!';
+      } else {
+        this.$router.push({name: 'menuRoute'});
+      }
+    },
+    removeQuiz: function () {
+      this.deleteQuiz();
+      this.$router.push({name: 'menuRoute'})
+    },
+    deleteQuiz: function () {
+      this.$http.delete("/quiz", {
+            params: {
+              quizId: this.quizId
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    getAllAnswers: function () {
+      this.$http.get("/questions/answers", {
+            params: {
+              questionId: this.questionId
+            }
+          }
+      ).then(response => {
+        this.answers = response.data
+        this.message = ''
+        this.emitClearAnswerRequest()
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    emitClearAnswerRequest: function () {
+      this.$emit('emitClearAnswerRequestEvent')
+    },
+    deleteAnswer: function (answerId) {
+
+      this.$http.delete("/questions/answer", {
+            params: {
+              answerId: answerId
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+        this.getAllAnswers();
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+  },
+  beforeMount() {
+    this.getAllAnswers()
+  }
+}
+</script>
