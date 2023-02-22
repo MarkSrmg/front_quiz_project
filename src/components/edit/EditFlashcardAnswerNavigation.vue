fa1c0870d32fa80f1fd12f80541d9b5c01781f67
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-02-22 13:56:56 +0200

    delete quiz edit flashcardis eemaldatud

--- src/components/edit/EditFlashcardAnswerNavigation.vue
+++ src/components/edit/EditFlashcardAnswerNavigation.vue
@@ -3,9 +3,6 @@
     <div class="mb-3">
       <button v-on:click="finishQuiz" type="button" class="btn btn-dark">Finish quiz</button>
     </div>
-    <div class="mb-3">
-      <button v-on:click="removeQuiz" type="button" class="btn btn-dark">Delete quiz</button>
-    </div>
   </div>
 </template>
 <script>
@@ -20,22 +17,6 @@ export default {
   methods: {
     finishQuiz: function () {
       this.$router.push({name: 'menuRoute'})
-    },
-    removeQuiz: function () {
-      this.deleteQuiz();
-      this.$router.push({name: 'menuRoute'})
-    },
-    deleteQuiz: function () {
-      this.$http.delete("/quiz", {
-            params: {
-              quizId: this.quizId
-            }
-          }
-      ).then(response => {
-        console.log(response.data)
-      }).catch(error => {
-        console.log(error)
-      })
     },
   }
 }
